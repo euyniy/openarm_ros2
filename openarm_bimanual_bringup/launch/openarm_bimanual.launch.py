f3791187acc584b00f73def9c386c776926ffd18
euyniy <yy727498@hotmail.com>
2025-06-02 14:47:16 +0900

    Implement bimanual launch
    Remove deprecated config
    
    Remove unused and fix spacing

---
+++ openarm_bimanual_bringup/launch/openarm_bimanual.launch.py
@@ -0,0 +1,190 @@
+# Copyright 2025 Reazon Holdings, Inc.
+# Copyright 2024 Stogl Robotics Consulting UG (haftungsbeschränkt)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, TimerAction
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="openarm_bimanual_bringup",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="openarm_bimanual_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="openarm_bimanual_description",
+            description="Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="openarm_bimanual.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="false",
+            description="Start robot with mock hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="joint_trajectory_controller",
+            choices=["forward_position_controller",
+                     "joint_trajectory_controller"],
+            description="Robot controller type to start for both arms.",
+        )
+    )
+
+    # Initialize Arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+    robot_controller = LaunchConfiguration("robot_controller")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package),
+                 "urdf", description_file]
+            ),
+            " ",
+            "use_mock_hardware:=",
+            use_mock_hardware,
+            " ",
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare(runtime_config_package), "config", controllers_file]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz",
+         "robot_description.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="both",
+        parameters=[robot_description, robot_controllers],
+    )
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", "/controller_manager"],
+    )
+
+    # Spawn both arm controllers based on the controller type
+    robot_controller_spawners = [
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[["left_", robot_controller],
+                       "-c", "/controller_manager"],
+        ),
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=[["right_", robot_controller],
+                       "-c", "/controller_manager"],
+        )
+    ]
+
+    # Delay loading and activation of `joint_state_broadcaster` after start of ros2_control_node
+    delay_joint_state_broadcaster_spawner_after_ros2_control_node = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=control_node,
+            on_start=[
+                TimerAction(
+                    period=3.0,
+                    actions=[joint_state_broadcaster_spawner],
+                ),
+            ],
+        )
+    )
+
+    # Delay loading and activation of robot_controller_names after `joint_state_broadcaster`
+    delay_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
+    for i, controller in enumerate(robot_controller_spawners):
+        delay_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=(
+                        robot_controller_spawners[i - 1]
+                        if i > 0
+                        else joint_state_broadcaster_spawner
+                    ),
+                    on_exit=[controller],
+                )
+            )
+        ]
+
+    return LaunchDescription(
+        declared_arguments
+        + [
+            control_node,
+            robot_state_pub_node,
+            rviz_node,
+            delay_joint_state_broadcaster_spawner_after_ros2_control_node,
+        ]
+        + delay_robot_controller_spawners_after_joint_state_broadcaster_spawner
+    )
